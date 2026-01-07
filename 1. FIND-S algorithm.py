3727115e3e48bcce2234770b02677522f387e98a
Nirmal007160 <balamurugannirmal7@gmail.com>
2026-01-07 13:43:41 +0530

    Created using Colab

---
+++ 1. FIND-S algorithm.py
@@ -0,0 +1,70 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyNW+aiSRFn0xBRliWDyibOT",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Nirmal007160/CA-GITHUB-/blob/main/1.%20FIND-S%20algorithm.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 2,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "UkbOpSOZYeLP",
+        "outputId": "a4298c42-39e0-4ae5-a8b3-ee7d79da7c39"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "Result Hypothesis: ['Sunny', 'Warm', '?', 'Strong', '?', '?']\n"
+          ]
+        }
+      ],
+      "source": [
+        "def find_s_algorithm(training_data):\n",
+        "    hypothesis = training_data[0][:-1]\n",
+        "    for example in training_data:\n",
+        "        if example[-1] == 'Yes':\n",
+        "            for i in range(len(hypothesis)):\n",
+        "                if example[i] != hypothesis[i]:\n",
+        "                    hypothesis[i] = '?'\n",
+        "    return hypothesis\n",
+        "\n",
+        "training_data = [\n",
+        "    ['Sunny', 'Warm', 'Normal', 'Strong', 'Warm', 'Same', 'Yes'],\n",
+        "    ['Sunny', 'Warm', 'High', 'Strong', 'Warm', 'Same', 'Yes'],\n",
+        "    ['Rainy', 'Cold', 'High', 'Weak', 'Cool', 'Change', 'No'],\n",
+        "    ['Sunny', 'Warm', 'High', 'Strong', 'Cool', 'Change', 'Yes']\n",
+        "]\n",
+        "\n",
+        "result_hypothesis = find_s_algorithm(training_data)\n",
+        "print(\"Result Hypothesis:\", result_hypothesis)"
+      ]
+    }
+  ]
+}
